9cc841c18e03ef055ee0c16df131fd4865eb2dd0
Igor Developer <igor.amaral@iamaral.com.br>
2020-08-08 20:02:26 -0300

    Projeto Início

---
+++ app/Http/Controllers/ToDoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ToDo;
+use Illuminate\Http\Request;
+
+class ToDoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ToDo  $toDo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ToDo $toDo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ToDo  $toDo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ToDo $toDo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ToDo  $toDo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ToDo $toDo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ToDo  $toDo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ToDo $toDo)
+    {
+        //
+    }
+}
